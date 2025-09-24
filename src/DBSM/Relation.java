760bd3b7328e8c30626bfc4ba692001c85a32205
Girometti Mattia <mattia.girometti@cz.lan>
2025-09-24 08:05:45 +0200

    Initial commit

---
+++ src/DBSM/Relation.java
@@ -0,0 +1,258 @@
+package DBSM;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Relation {
+
+    private String name;
+    private String[] fields_name;
+    private ArrayList<String[]> data;
+
+    public Relation(String n, String[] fn) {
+
+        name = n;
+        fields_name = new String[fn.length];
+        System.arraycopy(fn, 0, fields_name, 0, fn.length);
+
+        data = new ArrayList<String[]>();
+
+    }
+    // inserisce una nuova riga nella re
+    public void insert(String[] row){
+        if(!isDuplicated(row)){
+            String[] r = new String[row.length];
+            System.arraycopy(row, 0, r, 0, r.length);
+            data.add(r);
+        }
+    }
+
+    // ritorna true se una riga con gli stessi valori è presente
+    public boolean isDuplicated(String[] new_row){
+        boolean equals = true;
+
+        for(String[] row : data){
+            equals = true;
+            for(int i = 0; i < row.length; i++){
+                if(!row[i].equals(new_row)) {
+                    equals = false;
+                    i = row.length;
+                }
+            }
+            if(equals) return true;
+        }
+
+        return false;
+    }
+
+    public void save(String path){
+        try{
+            PrintWriter writer = new PrintWriter(new FileWriter(path + name + ".csv"));
+
+            for(String fn : fields_name){
+                writer.print(fn + ",");
+            }
+            writer.println("");
+
+            for(String[] row : data){
+                for(String f : row){
+                    writer.print(f + ",");
+                }
+                writer.println("");
+            }
+
+            writer.flush();
+            writer.close();
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    public static Relation load(String path, String name){
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(path + name + ".csv"));
+            // legge la riga di intestazione
+            String line = reader.readLine();
+            String[] fn = line.split(",");
+            Relation res = new Relation(name, fn);
+
+            // Legge tutte le righe del database
+            String[] row;
+            while ((line = reader.readLine()) != null){
+                row = line.split(",");
+                res.insert(row);
+            }
+
+            reader.close();
+
+            return res;
+        }catch (IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        int length_row = 20 * fields_name.length + (fields_name.length - 1);
+        String res = printLine() + "\n";
+        res += "|" + String.format("%-" + length_row + "s", name) + "|\n";
+        res += printLine() + "\n";
+
+        res += "|";
+        for(int i = 0; i < fields_name.length; i++) {
+            res += String.format("%-20s", fields_name[i]) + "|";
+        }
+
+        res += "\n" + printLine() + "\n";
+
+        for(String[] row : data){
+            res += "|";
+            for(int i = 0; i < fields_name.length; i++){
+                res += String.format("%-20s", row[i]) + "|";
+            }
+            res += "\n" + printLine() + "\n";
+        }
+
+        return res;
+    }
+
+    private int getFieldIndex(String fn){
+        for(int i = 0; i < fields_name.length; i++){
+            if (fn.equals(fields_name[i])) return i;
+        }
+
+        return -1;
+    }
+
+    public Relation selection(String condition){
+        Relation res = new Relation("R1", fields_name);
+
+        //0 è campo, 1 è operatore, 2 è valore
+        String[] cond = condition.split(" ");
+        int fi = getFieldIndex(cond[0]);
+
+        if (fi != -1){
+            for(String[] row : data){
+                if (cond[1].equals("=")){
+                    if (cond[2].equals(row[fi])){
+                        res.insert(row);
+                    }
+                } else if (cond[1].equals("<>")) {
+                    if (cond[2].equals(row[fi])){
+                        res.insert(row);
+                    }
+                }
+
+            }
+        }
+
+        return res;
+    }
+
+    public Relation projection (String[] fn){
+        Relation res = new Relation("R1", fn);
+
+        // Recupero gli indici di tutte le colonne
+        // suppongo che le colonne esistano
+        int[] field_index = new int[fn.length];
+        for(int i = 0; i < field_index.length; i++){
+            field_index[i] = getFieldIndex(fn[i]);
+        }
+
+        //crea array di appoggio per nuova riga
+        String[] new_row = new String[fn.length];
+        // copio per ogni riga in data solo le colonne che mi interessano
+        // e inserisco la nuova riga nella nuova relazione
+        for(String[] row : data){
+            for(int i = 0; i < new_row.length; i++){
+                new_row[i] = row[field_index[i]];
+            }
+            res.insert(new_row);
+        }
+
+        return res;
+    }
+
+    // ritorna una nuova relazione con il nome della relazione e dei campi
+    public Relation rename(String name, String[] fn){
+        Relation res = new Relation(name, fn);
+
+        for(String[] row : data){
+            res.insert(row);
+        }
+
+        return res;
+    }
+
+    // ritorna una nuova relazione con i campi della prima e della seconda
+    // e in ogni riga i valori di ogni riga della relazione associati alle
+    // righe di r
+    public Relation crossProduct(Relation r){
+        // Creo l'array dei nuovi fields name
+        String[] fn = new String[fields_name.length + r.fields_name.length];
+        System.arraycopy(fields_name, 0, fn, 0, fields_name.length);
+        System.arraycopy(r.fields_name, 0, fn, fields_name.length, r.fields_name.length);
+
+        // Creo la nuova relazione
+        Relation res = new Relation("R1", fn);
+
+        // Creo un supporto per la nuova riga
+        String[] new_row = new String[fn.length];
+
+        for(String[] ra : data){
+            System.arraycopy(ra, 0, new_row, 0, ra.length);
+            for(String[] rb : r.data){
+                System.arraycopy(rb, 0, new_row, ra.length, rb.length);
+                res.insert(new_row);
+            }
+        }
+
+        return res;
+    }
+
+    // Ritorna una nuova relazione con i campi della relazione di origine
+    // e le righe della prima e della seconda senza duplicati
+    public Relation union (Relation r){
+        // Creo la nuova relazione
+        Relation res = new Relation("R", fields_name);
+
+        for(String[] row : data){
+            res.insert(row);
+        }
+        for(String[] row : r.data){
+            res.insert(row);
+        }
+
+        return res;
+    }
+
+    // Ritorna una nuova relazione con i campi della relazione di origine
+    // e le righe della prima meno gli elementi della seconda
+    public Relation difference(Relation r){
+        // Creo la nuova relazione
+        Relation res = new Relation("R", fields_name);
+
+        for(String[] row : data){
+            if (!r.isDuplicated(row)){
+                res.insert(row);
+            }
+        }
+
+        return res;
+    }
+
+    private String printLine(){
+        int length_row = 20 * fields_name.length + (fields_name.length-1);
+        String res = "+";
+        res += String.format("%-" + length_row + "s", "").replace(" ", "-");
+        res += "+";
+        return res;
+    }
+}
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import DBSM.Relation;
+
+public class Main {
+    public static void main(String[] args){
+
+        /*
+        String[] fn = {"Cognome", "Nome", "Data", "MateriaP"};
+        Relation r = new Relation("Studente", fn);
+
+        String[] s = {"Rossi", "Mario", "1-12-2000", "Italiano"};
+        r.insert(s);
+        String[] s1 = {"Bellardi", "Michele", "7-8-2007", "Inglese"};
+        r.insert(s1);
+        String[] s2 = {"Rigo", "Jacopo", "25-5-2007", "Informatica"};
+        r.insert(s2);
+
+        r.save("./");
+        */
+
+        Relation r = Relation.load("./", "Studente");
+
+        Relation r1 = Relation.load("./", "Aule");
+
+        Relation r2 = Relation.load("./", "Docenti");
+
+        if(r != null && r1 != null && r2 != null){
+            System.out.println("PROVA SELECTION");
+            System.out.println(r.selection("MateriaP = Italiano"));
+
+            System.out.println("PROVA PROJECTION");
+            System.out.println(r.projection(new String[]{"Nome", "Cognome"}));
+
+            System.out.println("PROVA CROSSPRODUCT");
+            //System.out.println(r1.rename("Cicci", new String[]{"Paolino", "Paperino"}));
+            System.out.println(r.crossProduct(r1));
+
+            System.out.println("PROVA UNION"); // ERRORE! UNION NON FUNZIONA
+            Relation ru = r.union(r2);
+
+            System.out.println(ru);
+
+            System.out.println("PROVA DIFFERENCE");
+            System.out.println(ru.difference(r));
+
+        }
+
+
+
+        //
+    }
+}
